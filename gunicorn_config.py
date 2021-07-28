bb42fa560d54502ab07759e918938a78e512718b
ahmetkca <ahmet.karapinar@ontariotechu.net>
2021-07-28 00:23:13 -0400

    Moved the registration of scheduled jobs to gunicorn startup rather than fastapi startup.

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,9 +1,36 @@
+from MyScheduler import MyScheduler
+from database import MongoDB, MyRedis
+from config import SCHEDULED_JOB_INTERVAL
+from EtsyShopManager import EtsyShopManager
+
+
 def on_starting(server):
     """
     Do something on server start
     """
+    mongodb = MongoDB()
+    r = MyRedis().r
     print("Server has started")
     print("Gunicorn on_starting event is working................")
+    myScheduler = MyScheduler(mongodb)
+    myScheduler.scheduler.start()
+    etsy_connections = await mongodb.db["EtsyShopConnections"].find().to_list(100)
+    job_offset = 0
+    for etsy_connection in etsy_connections:
+        _id = str(etsy_connection["_id"])
+        myScheduler.scheduler.add_job(
+            EtsyShopManager.syncShop,
+            "interval",
+            minutes=SCHEDULED_JOB_INTERVAL + job_offset,
+            kwargs={"etsy_connection_id": _id,
+                    "db": mongodb.db,
+                    "r": r},
+            id=f"{_id}:syncShopProcess",
+            name=f"{_id}:syncShopProcess",
+            # jobstore="mongodb"
+        )
+        job_offset += 5
+    myScheduler.scheduler.print_jobs()
 
 
 def on_reload(server):
--- main.py
+++ main.py
@@ -47,33 +47,33 @@ print(origins)
 app = FastAPI()
 
 
-@app.on_event("startup")
-async def startup_event():
-	myScheduler = MyScheduler(mongodb)
-	myScheduler.scheduler.start()
-	etsy_connections = await mongodb.db["EtsyShopConnections"].find().to_list(100)
-	job_offset = 0
-	for etsy_connection in etsy_connections:
-		_id = str(etsy_connection["_id"])
-		myScheduler.scheduler.add_job(
-			EtsyShopManager.syncShop,
-			"interval",
-			minutes=SCHEDULED_JOB_INTERVAL + job_offset,
-			kwargs={"etsy_connection_id": _id,
-			        "db": mongodb.db,
-			        "r": r},
-			id=f"{_id}:syncShopProcess",
-			name=f"{_id}:syncShopProcess",
-			# jobstore="mongodb"
-		)
-		job_offset += 5
-	myScheduler.scheduler.print_jobs()
-	
+# @app.on_event("startup")
+# async def startup_event():
+# 	myScheduler = MyScheduler(mongodb)
+# 	myScheduler.scheduler.start()
+# 	etsy_connections = await mongodb.db["EtsyShopConnections"].find().to_list(100)
+# 	job_offset = 0
+# 	for etsy_connection in etsy_connections:
+# 		_id = str(etsy_connection["_id"])
+# 		myScheduler.scheduler.add_job(
+# 			EtsyShopManager.syncShop,
+# 			"interval",
+# 			minutes=SCHEDULED_JOB_INTERVAL + job_offset,
+# 			kwargs={"etsy_connection_id": _id,
+# 			        "db": mongodb.db,
+# 			        "r": r},
+# 			id=f"{_id}:syncShopProcess",
+# 			name=f"{_id}:syncShopProcess",
+# 			# jobstore="mongodb"
+# 		)
+# 		job_offset += 5
+# 	myScheduler.scheduler.print_jobs()
 
 
 @app.on_event("shutdown")
 async def shutdown_event():
 	await mongodb.client.close()
+	r.close()
 
 
 @app.get("/")
