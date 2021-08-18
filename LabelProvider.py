3a59ae10fcb74af497f79d322c71dba3844a0f3d
ahmetkca <ahmet.karapinar@ontariotechu.net>
2021-08-18 03:26:24 -0400

    Added new function for getting stallion label directly from stallion api.

--- LabelProvider.py
+++ LabelProvider.py
@@ -65,6 +65,55 @@ class StallionCsvFileManager:
 			pass
 
 
+class PurchaseLabelData:
+	def __init__(self, 
+		name: str,
+		order_id: str,
+		address1: str,
+		address2: str,
+		city: str,
+		province_code: str,
+		country_code: str,
+		postal_code: str,
+		package_contents: str,
+		store: str
+	):
+		self.name = name
+		self.order_id = order_id,
+		self.address1 = address1,
+		self.address2 = address2,
+		self.city = city,
+		self.province_code = province_code,
+		self.country_code = country_code,
+		self.postal_code = postal_code,
+		self.package_contents = package_contents
+		self.store = store
+
+
+class DirectlyFromStallionAPI(LabelProvider):
+	@staticmethod
+	async def get_label_by_receipt_id(receipt_id: str):
+		"""
+			Retriving label directly from StallionExpress API
+		"""
+		stallion_api: StallionAPI = StallionAPI(api_token=STALLION_API_TOKEN)
+		res: dict = await stallion_api.get_status_by_order_id(order_id=receipt_id)
+		if res is not None and res.get('success'):
+			try:
+				shipment_result = await stallion_api.get_shipment(res.get('details').get('ship_code'))
+			except LabelNotFound as e:
+				logging.error(e)
+				return None
+			else:
+				return Base64ToBytes.base64_to_bytes(shipment_result['label'])
+		else:
+			return None
+
+	@staticmethod
+	def purchase_label(purchaseLabelData: PurchaseLabelData):
+		purc
+
+
 class StallionLabelManager(LabelProvider):
 	@staticmethod
 	async def get_label_by_receipt_id(receipt_id: int):
@@ -121,8 +170,38 @@ class StallionAPI:
 			res = await client.get(f'{STALLION_API_BASE_URL}/track', headers=headers, params=params)
 			return res.json()
 
+	async def purchase_label(self, purchaseLabelData: PurchaseLabelData ):
+		async with AsyncClient() as client:
+			headers = {
+				"Authorization": f"Bearer {self.api_token}",
+				'accept': 'application/json'
+			}
+			params = purchaseLabelData.dict()
+			params["is_fba"] = False
+			params["signature_confirmation"] = False
+			params["weight_unit"] = "lbs"
+			params["size_unit"] =  "in"
+			params["value"] =  20
+			params["currency"] = "CAD"
+			params['weight'] = 0.2
+			params["length"]= 5
+			params["width"]= 5
+			params["height"] = 5
+			params["label_format"] = "pdf"
+			params["purchase_label"] = True
+			params["insured"] = True,
+			params["needs_postage"] = True
+			if purchaseLabelData.get('country_code') == 'US':
+				params["package_type"] = "thick_envelope"
+				params["postage_type"]= "usps_first_class_mail"
+			elif purchaseLabelData.get('country_code') == 'CA':
+				params["package_type"] = "thick_envelope"
+				params["postage_type"] = "stallion_express_domestic"
+				
 
 class LabelNotFound(Exception):
 	def __init__(self, shipment_id):
 		self.shipment_id = shipment_id
 		super().__init__(shipment_id)
+
+
