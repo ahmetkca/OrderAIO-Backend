f565b22abd1c7c6836acd9983fdf8f87a6f66bfc
ahmetkca <ahmet.karapinar@ontariotechu.net>
2021-08-18 03:30:48 -0400

    FINISHED: the new search endpoint which uses aggregate future of the mongodb

--- endpoints/new_search.py
+++ endpoints/new_search.py
@@ -32,7 +32,85 @@ async def new_search(
 	search_body: NewSearchBody,
 	user: UserData = Depends(is_authenticated) 
 ):
-	search_body = search_body.dict()
-	search_body = {k: search_body[k] for k in search_body.keys() if search_body[k] is not None}
+	# search_body = search_body.dict()
+	# search_body = {k: search_body[k] for k in search_body.keys() if search_body[k] is not None}
 	pprint.pprint(search_body)
-	return search_body
+	collation = {}
+	match = {}
+	project = {
+		# "Note": False,
+		# "Note._id": False,
+		"_id": False,
+		# "receipt_id": True
+		"Note": True
+	}
+	if search_body.receipt_status is not None:
+		match['Note.status'] = search_body.receipt_status
+	if search_body.shop_name is not None:
+		match['shop_name'] = search_body.shop_name
+	if search_body.from_date is not None and search_body.to_date is not None:
+		match["max_due_date"] = {
+			"$lte": search_body.to_date,
+			"$gte": search_body.from_date
+		}
+	elif search_body.from_date is not None and search_body.to_date is None:
+		match["max_due_date"] = {
+			"$lte": search_body.from_date,
+			"$gte": search_body.from_date
+		}
+	elif search_body.from_date is None and search_body.to_date is not None:
+		match["max_due_date"] = {
+			"$lte": search_body.to_date,
+			"$gte": search_body.to_date
+		}
+	if search_body.query is not None:
+		is_int: bool = False
+		try:
+			int(search_body.query)
+		except ValueError:
+			is_int = False
+		else:
+			search_body.query = int(search_body.query)
+			is_int = True
+		if is_int:
+			match["receipt_id"] = search_body.query
+		else:
+			match["name"] = search_body.query
+			collation["locale"] = "en"
+			collation["strength"] = 1
+	if search_body.projection is not None and len(search_body.projection) > 0:
+		for p in search_body.projection:
+			if p == "_id":
+				continue
+			project[p] = True
+	print(match)
+	print(project)
+
+	mongodb_search_result = await mongodb.db['Receipts'].aggregate([
+		{
+			'$lookup': {
+				'from': 'Notes', 
+				'localField': 'receipt_id', 
+				'foreignField': 'receipt_id', 
+				'as': 'Note'
+			}
+		}, {
+			'$match': {**match}
+		}, {
+			'$unwind': {
+				'path': '$Note',
+				'preserveNullAndEmptyArrays': True
+			}
+		},{
+			"$project": {**project}
+		}, {
+			"$unset": ['Note._id']
+		}
+	]).to_list(100)
+	# print()
+	# search_result = []
+	# async for receipt in mongodb_search_result:
+	# 	search_result.append(receipt)
+	return mongodb_search_result
+
+
